cf4663aa7009786486ca55cb7f1f0326255b3ee1
Stephen Arnold <smarnold@me.com>
2021-07-03 08:49:39 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,10 +14,23 @@ radio.onReceivedValue(function (name, value) {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(value))
         }
     } else if (name == "Obstacle") {
-    	
+        Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+        distance = Kitronik_Move_Motor.measure()
+        if (distance >= 20) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 100)
+        } else if (distance < 20 && distance < 5) {
+            basic.pause(500)
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
+            basic.pause(1000)
+            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
+            basic.pause(500)
+        } else {
+            Kitronik_Move_Motor.stop()
+        }
     } else {
         Kitronik_Move_Motor.stop()
     }
 })
+let distance = 0
 basic.showString("R")
 radio.setGroup(1)
