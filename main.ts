be1f5b38399ba249ac8836dc6ae13ca7b2c0c78b
Stephen Arnold <smarnold@me.com>
2021-07-02 12:06:28 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "LMotor") {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, value)
+        if (value >= 0) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value)
+        } else {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(value))
+        }
+    } else if (name == "RMotor") {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, Math.abs(value))
+        if (value >= 0) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, value)
+        } else {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(value))
+        }
+    } else {
+        Kitronik_Move_Motor.stop()
+    }
+})
+basic.showString("R")
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
