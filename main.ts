45a310d235d2fbfd6a0d86090a67120d5b498f5c
Stephen Arnold <smarnold@me.com>
2021-07-03 10:35:25 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "LMotor") {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, value)
         if (value >= 0) {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value)
         } else {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(value))
         }
     } else if (name == "RMotor") {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, value)
         if (value >= 0) {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, value)
         } else {
@@ -17,12 +15,10 @@ radio.onReceivedValue(function (name, value) {
         Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
         distance = Kitronik_Move_Motor.measure()
         if (distance >= 20) {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 100)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, value)
         } else if (distance < 20 && distance < 5) {
             basic.pause(500)
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 50)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 50)
             basic.pause(1000)
