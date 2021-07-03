b1fb8f8a04c630ddecd7d65cc8d3bcfcfe80417d
Stephen Arnold <smarnold@me.com>
2021-07-03 17:31:19 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,14 +12,13 @@ radio.onReceivedValue(function (name, value) {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(value))
         }
     } else if (name == "Obstacle") {
+        distance = Kitronik_Move_Motor.measure()
         basic.showString("" + (distance))
         if (distance >= 20) {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, value)
-        } else if (distance < 20 && distance < 5) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
+        } else if (distance < 20 && distance > 5) {
             basic.pause(500)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 50)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 50)
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
             basic.pause(1000)
             Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
             basic.pause(500)
@@ -36,4 +35,3 @@ let distance = 0
 radio.setGroup(1)
 basic.showString("R")
 Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
-distance = Kitronik_Move_Motor.measure()
