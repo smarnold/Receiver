3dd544bd9bf8830f299c20d6417ec456270d4f12
Stephen Arnold <smarnold@me.com>
2021-07-03 10:15:57 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,9 +18,13 @@ radio.onReceivedValue(function (name, value) {
         distance = Kitronik_Move_Motor.measure()
         if (distance >= 20) {
             Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 100)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, value)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, value)
         } else if (distance < 20 && distance < 5) {
             basic.pause(500)
             Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 50)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, 50)
             basic.pause(1000)
             Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 50)
             basic.pause(500)
