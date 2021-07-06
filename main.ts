38bf1c7a95e4b3cda3cfa20f10e28e70678960d4
Stephen Arnold <smarnold@me.com>
2021-07-07 09:16:10 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+function indicate (direction: string) {
+    indicating = true
+    if (direction == "left") {
+        for (let index = 0; index < 4; index++) {
+            moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+            moveMotorZIP.setZipLedColor(3, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+            moveMotorZIP.show()
+            basic.pause(200)
+            moveMotorZIP.clear()
+            moveMotorZIP.show()
+            basic.pause(200)
+        }
+    } else if (direction == "right") {
+        for (let index = 0; index < 4; index++) {
+            moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+            moveMotorZIP.setZipLedColor(2, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+            moveMotorZIP.show()
+            basic.pause(200)
+            moveMotorZIP.clear()
+            moveMotorZIP.show()
+            basic.pause(200)
+        }
+    } else {
+        indicating = false
+    }
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "LMotor") {
         if (value >= 0) {
@@ -29,9 +55,23 @@ radio.onReceivedValue(function (name, value) {
         basic.showString("R")
     }
 })
+let indicating = false
 let distance = 0
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
+moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 radio.setGroup(1)
 basic.showString("R")
 Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
 distance = Kitronik_Move_Motor.measure()
 basic.showString("" + (distance))
+let headlights = moveMotorZIP.range(0, 2)
+let rearlights = moveMotorZIP.range(2, 2)
+basic.forever(function () {
+    if (input.lightLevel() < 200) {
+        headlights.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+        rearlights.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    } else {
+        moveMotorZIP.clear()
+        moveMotorZIP.show()
+    }
+})
